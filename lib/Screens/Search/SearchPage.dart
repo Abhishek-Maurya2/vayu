545b7aa1b905d37bde588c74b7495b62699bb976
Abhishek-Maurya2 <208akmaurya@gmail.com>
2023-04-19 18:52:54 +0530

    commit 1

--- lib/Screens/Search/SearchPage.dart
+++ lib/Screens/Search/SearchPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:vayu/Widgets/CustomAppDrawer.dart';
+import 'package:vayu/Widgets/CustomBottomBar/BottomBarCOntroller.dart';
+import 'package:vayu/Widgets/CustomBottomBar/CustomBottomBar.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -6,8 +9,47 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(child: Text("ITS SEARCH PAGE ")),
+      drawer: CustomAppDrawer(),
+      appBar: AppBar(
+        title: Text("वायु",
+            style: TextStyle(
+              color: Color.fromRGBO(122, 122, 122, 1),
+            )),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        foregroundColor: Colors.black,
+      ),
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(10),
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black,
+                          size: 20,
+                        ),
+                        prefixIconConstraints:
+                            BoxConstraints(maxHeight: 20, minWidth: 25),
+                        border: InputBorder.none,
+                        hintText: 'Search',
+                        hintStyle: TextStyle(color: Colors.grey)),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
